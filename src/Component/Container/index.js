fa9e62b1c28538aac530b86bc1c5ec7b1b907774
Olivier Colonna <olivier.colonna@drivetribe.com>
2017-07-13 16:11:52 +0100

    Container component and setup

---
+++ src/Component/Container/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import styles from './styles.css';
+
+/**
+ * Class Container
+ * @author oliviercolonna@gmail.com
+ */
+class Container extends Component {
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    vertical: PropTypes.bool,
+    horizontal: PropTypes.bool,
+    className: PropTypes.string,
+  }
+
+  render = () => {
+    const { children, vertical, horizontal, className, ...restProps } = this.props;
+    const containerClass = classnames({
+      [styles.container]: true,
+      [styles.containerScrollY]: vertical,
+      [styles.containerScrollX]: horizontal,
+      [className]: className,
+    });
+
+    return (
+      <div className={containerClass} {...restProps}>
+        { children }
+      </div>
+    );
+  }
+}
+
+export default Container;
